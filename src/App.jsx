b7d2df08085f781f4271303bca78398151520d95
alwaw <124403241+alwaw@users.noreply.github.com>
2024-09-23 21:17:55 +0200

    github pages - test

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import TitlesDisplay from './components/TitlesDisplay/TitlesDisplay';
 import Layout from './components/Layout/Layout.jsx';
 import AddNew from './components/AddNew/AddNew.jsx';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
+import About from './pages/About/About.jsx';
 
 
 const router = createBrowserRouter(
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
     </>
   )
   
-  
+ 
 )
 
 function App() {
